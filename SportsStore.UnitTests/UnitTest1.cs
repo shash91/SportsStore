0c76a40f751ecbd4f04ee7e7f7ce2813a899f488
shash91 <shashika@davton.com>
2016-04-29 13:42:27 +0530

    Displaying the page links and unit tests for them
    Changes includes creating page links(pagination) and unit testing is for
    checking to ensure that the correct data is sent by controller to the
    view

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace SportsStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        /*   public void Can_Paginate()
+           public void Can_Paginate()
            {
                //Arrange
                Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
@@ -31,15 +31,16 @@ namespace SportsStore.UnitTests
                ProductController con = new ProductController(mock.Object);
                con.PageSize = 3;
 
-               //ACt
-               IEnumerable<Product> result = (IEnumerable<Product>)con.List(2).Model;
+            //ACt
+            ProductsListViewModel result = (ProductsListViewModel)con.List(2).Model;
 
                //Assert
-               Product[] arr = result.ToArray();
+               Product[] arr = result.Products.ToArray();
                Assert.IsTrue(arr.Length == 2);
                Assert.AreEqual(arr[0].Name , "P4");
                Assert.AreEqual(arr[1].Name, "P5");
-               }*/
+               }
+        [TestMethod]
 
         public void Can_Generate_Page_Links()
         {
@@ -62,6 +63,30 @@ namespace SportsStore.UnitTests
             + @"<a class=""selected"" href=""Page2"">2</a>"
             + @"<a href=""Page3"">3</a>");
 
+        }
+        [TestMethod]
+        public void Can_Send_Pagination_View_model()
+        {
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+
+                {
+                    new Product {ProductID=1,Name="P1" },
+                    new Product {ProductID=2,Name="p2" },
+                    new Product {ProductID=3,Name="p3" },
+                    new Product {ProductID=4,Name="p4" },
+                    new Product {ProductID=5,Name="P5" }
+                }.AsQueryable());
+
+            ProductController con = new ProductController(mock.Object);
+            con.PageSize = 3;
+            ProductsListViewModel result = (ProductsListViewModel)con.List(2).Model;
+
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.Totalpages, 2);
         }
     }
 }
