f07f03fed0f9af051028db5921ebde3eb851c039
shash91 <shashika@davton.com>
2016-05-03 15:35:12 +0530

    Getting Shipping details from customer
    A View for inserting shipping details and implemented shippingDetails
    model that represents the shipping details for a customer

---
+++ SportsStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Domain.Entities
+{
+    public class ShippingDetails
+    {
+
+        [Required(ErrorMessage ="Please Enter a name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please Enter the first Address line")]
+        public string line1 { get; set; }
+        public string line2 { get; set; }
+        public string line3{ get; set; }
+
+        [Required(ErrorMessage ="Please enter a city name")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please enter a state name")]
+        public string State { get; set; }
+        public string zip { get; set; }
+
+        [Required(ErrorMessage = "Please enter a country name")]
+        public string Country { get; set; }
+        public bool GiftWrap { get; set; }
+
+
+    }
+}
--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -52,6 +52,10 @@ namespace SportsStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
        
 
     }
